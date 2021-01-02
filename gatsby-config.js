cc21a9bd7b581268679254b2b253608441f7d2dc
alvaro <me@alvaromb.com>
2021-01-02 12:02:48 +0100

    Configured Plausible analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,12 +45,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -70,6 +64,13 @@ module.exports = {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography',
+      },
+    },
+    // Analytics
+    {
+      resolve: `gatsby-plugin-plausible`,
+      options: {
+        domain: `alvaromb.com`,
       },
     },
   ],
