0784bb22d60119b994e31e533d9c57698e009f0f
alvaro <me@alvaromb.com>
2021-04-10 22:38:23 +0200

    Started about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,24 @@ const About = () => {
   return (
     <Layout>
       <h2>About</h2>
+      I'm a software engineer
+      <a href="#fn-uib">
+        <sup>1</sup>
+      </a>{' '}
+      and a manager
+      <a href="#fn-pdd">
+        <sup>2</sup>
+      </a>{' '}
+      living in Palma, Spain.
+      <ol>
+        <li id="fn-uib">
+          BSc., MSc. from Universitat de les Illes Balears (Spain).
+        </li>
+        <li id="fn-pdd">
+          PDD, Program for Management Development from IESE Business School
+          (Spain).
+        </li>
+      </ol>
     </Layout>
   )
 }
